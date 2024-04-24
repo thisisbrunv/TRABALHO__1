69fdff51db1167792a62f74772ad911d4bfdd9bb
HugoLSouza99 <110997671+HugoLSouza99@users.noreply.github.com>
2024-04-24 10:36:34 -0300

    Tentativa de Barra de pesquisa

--- lib/main.dart
+++ lib/main.dart
@@ -176,6 +176,17 @@ void carregarClientesSalvos() async {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => BarraPesquisa(prestadores: [],),
+                        ),
+                      );
+                    },
+                    child: const Text('Ir para Barra de Pesquisa'),
+                  ),
+                    ElevatedButton(
                         onPressed: () {
                           removerTudo();
                         },
